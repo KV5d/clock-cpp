3f8531c332f29c7521c064eda46986c3cbd955cf
KV5d <raineberrigan@outlook.com>
2022-11-07 22:38:19 -0700

    2

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+
+
 #include<cstdio>
 #include<iostream>
 #include<string>
@@ -5,8 +7,15 @@
 using namespace std;
 
 int main() {
-    string clock = "clock";
-    printf("%s\n", clock.c_str());
+    string clock = "\033[35;1mclock\033[0m";
+    string spacer = "\033[36;5m----------------\033[0m";
+    printf("\n%s\n%s\n", clock.c_str(), spacer.c_str());
+
+    printf("\033[38;1;3m");
+    system("date");
+    printf("\033[0m");
+
+    printf("%s\n", spacer.c_str());
 
     return 0;
 }
